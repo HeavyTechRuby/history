32a2e7bd224511580d28912362bf7184ba111cf9
Sergei O. Udalov <udalov.x@mail.ru>
2024-01-18 08:27:44 +0300

    patch_01 08:27

--- features/step_definitions/basics.rb
+++ features/step_definitions/basics.rb
@@ -1,5 +1,9 @@
 When('I am a guest') do
-  context.set_user Guest.new
+  context.set_user Testing::Guest.new
+end
+
+Given('I am a user') do
+  context.set_user Testing::User.new
 end
 
 When('I open the main page') do
@@ -9,3 +13,13 @@ end
 When('I see text {string}') do |text|
   expect(page).to have_content text
 end
+
+
+When('I click on {string}') do |item|
+  click_on item
+end
+
+When('fill in {string} with {string}') do |item, text|
+  fill_on item, with: text
+end
+
--- features/support/custom.rb
+++ features/support/custom.rb
@@ -1,18 +1,23 @@
 require "dotenv"
 Dotenv.load(".env.test.local", ".env.local", ".env.test", ".env")
 
-class Guest
-end
+module Testing
+  class Guest
+  end
 
-class Context
-  def set_user(new_user)
-    @current_user = new_user
+  class User
+  end
+
+  class Context
+    def set_user(new_user)
+      @current_user = new_user
+    end
   end
 end
 
 module History
   module WorldHelpers
-    def context = @context ||= Context.new
+    def context = @context ||= Testing::Context.new
   end
 end
 
