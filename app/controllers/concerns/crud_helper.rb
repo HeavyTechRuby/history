c365aaa9ea343bf79c7e13f3c000f9054c6d1b99
Alex Abramov <alexanderabramov24@gmail.com>
2024-07-04 21:13:09 +0700

    locale fix crud helper

--- app/controllers/concerns/crud_helper.rb
+++ app/controllers/concerns/crud_helper.rb
@@ -6,7 +6,7 @@ module CrudHelper
   def update_record(record, params, redirect_url)
     respond_to do |format|
       if record.update(params)
-        format.html { redirect_to redirect_url, notice: "#{record.model_name.human} was successfully updated." }
+        format.html { redirect_to redirect_url, notice: t("helpers.crud.update_success") }
         format.json { render :show, status: :ok, location: record }
       else
         format.html { render :edit, status: :unprocessable_entity }
